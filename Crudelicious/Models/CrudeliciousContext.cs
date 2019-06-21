901889f3e2b66e8a02b69e392965f11b2a598f80
mbower386 <mbower386@gmail.com>
2019-06-20 19:32:38 -0700

    getting started with entity framework

---
+++ Crudelicious/Models/CrudeliciousContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Crudelicious.Models
+{
+    public class CrudeliciousContext : DbContext
+    {
+        // base() calls the parent class' constructor passing the "options" parameter along
+        public CrudeliciousContext (DbContextOptions options) : base (options) { }
+    }
+}
---
+++ Crudelicious/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Crudelicious.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Crudelicious
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices (IServiceCollection services)
+        {
+            string mySqlConnection = "server=localhost;userid=root;password=root;port=3306;database=mydb;SslMode=None";
+            services.AddDbContext<CrudeliciousContext> (options => options.UseMySql (mySqlConnection));
+            services.AddSession ();
+            services.AddMvc ();
+        }
+
+        public Startup (IHostingEnvironment env)
+        {
+            Console.WriteLine (env.ContentRootPath);
+            Console.WriteLine (env.IsDevelopment ());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment ())
+            {
+                app.UseDeveloperExceptionPage ();
+            }
+
+            app.UseSession ();
+            app.UseStaticFiles ();
+            app.UseMvc ();
+        }
+    }
+}
